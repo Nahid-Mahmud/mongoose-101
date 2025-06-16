96acf66555c8e1158064604950dd4d8327bd4906
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-16 23:03:55 +0600

    feat: enhance user creation and retrieval routes with error handling and type definitions

--- src/app/controllers/users.controllers.ts
+++ src/app/controllers/users.controllers.ts
@@ -1,11 +1,47 @@
-import express, { Router } from "express";
+import express, { Request, Response, Router } from "express";
 import { User } from "../models/user.model";
 
 export const userRoutes = Router();
 
+// crate user
+
+userRoutes.post("/create-user", async (req: Request, res: Response) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    // check if the user already exists
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists" });
+    }
+    // create a new user
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: savedUser,
+    });
+  } catch (error: unknown) {
+    console.error("Error creating user:", error);
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "An unknown error occurred" });
+    }
+  }
+});
+
 // get all users
 
-userRoutes.get("/", async (req, res) => {
+userRoutes.get("/", async (req: Request, res: Response) => {
   try {
     const users = await User.find().select("-password");
     if (!users || users.length === 0) {
@@ -21,6 +57,3 @@ userRoutes.get("/", async (req, res) => {
     }
   }
 });
-
-
-
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,16 +2,22 @@
 import { Schema, model } from "mongoose";
 import { IUser, UserRole } from "../interface/user.interface";
 
-const userSchema = new Schema<IUser>({
-  firstName: { type: String, required: true, trim: true },
-  lastName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true, lowercase: true },
-  password: { type: String, required: true },
-  role: {
-    type: String,
-    enum: UserRole,
-    default: UserRole.user,
+const userSchema = new Schema<IUser>(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: UserRole,
+      default: UserRole.user,
+    },
   },
-});
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 export const User = model<IUser>("User", userSchema);
