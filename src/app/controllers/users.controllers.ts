8c2b39c3edb3eb231d0647ec3588ae487dd3bd7b
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-16 23:21:32 +0600

    feat: add delete user route with error handling

--- src/app/controllers/users.controllers.ts
+++ src/app/controllers/users.controllers.ts
@@ -101,3 +101,27 @@ userRoutes.get("/:id", async (req: Request, res: Response) => {
     }
   }
 });
+
+// delete user
+userRoutes.delete("/:id", async (req: Request, res: Response) => {
+  const userId = req.params.id;
+
+  try {
+    // check if the user exists
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({
+      message: "User deleted successfully",
+      user: deletedUser,
+    });
+  } catch (error: unknown) {
+    console.error("Error deleting user:", error);
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "An unknown error occurred" });
+    }
+  }
+});
